db1c4490eeb54ccf1324a818043c6b1d8e0897e3
michaevelli <michael.pc168@gmail.com>
2022-01-18 15:36:07 +1100

    added stastistics

--- README.md
+++ README.md
@@ -8,7 +8,19 @@ Algorithm is Minimax. It begins with the restricted word bank of possible answer
 A run of the algorithm with the word bank and possible answers gives the following optimal starting guesses: `ARISE`, `RAISE`, `AESIR` and `SERAI`. Only the first 2 are in the possible word bank so rather than wait 2 hours for a first guess that will always be the same word, `RAISE` is hardcoded as the first guess.
 A first pass of the algorithm has been performed for every possible result. The subsequent guess for each result has been stored and is retrieved to reduce execution time (Second guesses typically take around 10 minutes otherwise). A significant number of second guesses are `AAHED` because the result and `RAISE` do not provide any compatible words. Eg, `YYYGG` and `RAISE` do not result in any words in the possible answers word bank. Hence, the code I used to generate second guesses defaulted to the first word in the word bank, `AAHED`. At this point the possible words set is usually around 10 words max which takes the algorithm about 1 minute to execute.
 
-From a small sample size of testing, I believe the algorithm will take no more than 5 guesses to guess the word.
+Running the algorithm on all possible words give the following results
+
+| Guesses Required | Occurences | Percentage |
+|-----------------:|-----------:|-----------:|
+|                1 |          1 |      0.04% |
+|                2 |         67 |      2.89% |
+|                3 |        953 |     41.17% |
+|                4 |       1206 |     52.10% |
+|                5 |         88 |      3.80% |
+|                6 |          0 |      0.00% |
+
+This gives this algorithm an average case of 3.57 turns to guess the word, with a worst case of 5 turns.
+
 
 ## Usage
 
---
+++ helpers/generatetestresults.py
@@ -0,0 +1,122 @@
+def splitString(s):
+    return [c for c in s]
+
+
+def query(answer, guess):
+    result = ["-", "-", "-", "-", "-"]
+    answerlist = splitString(answer)
+    guesslist = splitString(guess)
+    for i in range(len(guesslist)):
+        if guesslist[i] == answerlist[i]:
+            result[i] = "G"
+            answerlist[i] = " "
+    for i in range(len(guesslist)):
+        if result[i] == "G":
+            continue
+        if guesslist[i] in answerlist:
+            result[i] = "Y"
+            answerlist[answerlist.index(guesslist[i])] = " "
+    return "".join(result)  
+
+# Get word bank
+f = open("wordbank.txt")
+wordBank = f.read().split(" ")
+f.close()
+f = open ("secondguesses.txt")
+second = f.read().split(" ")
+f.close()
+f = open("wordbankanswers.txt")
+testbank = f.read().split(" ")
+f.close()
+
+
+
+for x in range(0, len(testbank)):
+    testcase = testbank[x]
+    
+
+    f = open("wordbankanswers.txt")
+    possible = f.read().split(" ")
+    f.close()
+
+    # Prepare lists
+    scores = []
+    for i in range(len(wordBank)):
+        scores += [0]
+
+    oldguesses = []
+
+    f = open("possibleresults.txt")
+    possibleResults = f.read().split(" ")
+    f.close()
+
+    # Running minmax immediately gives 4 words to start with: ARISE, RAISE, aesir and serai. Only the first 2 are in the possible words set
+    guess = "RAISE"
+    oldguesses += [guess]
+    result = query(testcase, guess)
+
+    while (result != "GGGGG"):
+        # Prune possible words
+        tempPossible = []
+        for word in possible:
+            testResult = query(word, guess)
+            if result == testResult:
+                tempPossible += [word]
+        possible = tempPossible
+        
+ 
+        if len(possible) == 0:
+            guess = "-----"
+            break
+
+        if (len(possible) < 3):
+            guess = possible[0]
+            oldguesses += [guess]
+            result = query(testcase, guess)
+            continue
+        
+        # Obtain second guess from pregenerated word bank
+        if (guess == "RAISE"):
+            guess = second[possibleResults.index(result)]
+            oldguesses += [guess]
+            result = query(testcase, guess)
+            continue
+
+
+        # MinMax
+        for i in range(len(wordBank)):
+            word = wordBank[i]
+            if word in oldguesses:
+                scores[i] = 0
+                continue
+            scores[i] = len(wordBank)
+            for possibleresult in possibleResults:
+                countmin = 0
+                for possibleword in possible:
+                    possiblewordresult = query(possibleword, word)
+                    if possiblewordresult != possibleresult:
+                        countmin += 1
+                if scores[i] > countmin:
+                    scores[i] = countmin
+        
+        # Guess the Max of the Min
+        highscores = [i for i, j in enumerate(scores) if j == max(scores)]
+        guess = wordBank[scores.index(max(scores))]
+
+        # try to use a guest in the possible set
+        for i in highscores:
+            if wordBank[i] in possible:
+                guess = wordBank[i]
+                break
+        
+        oldguesses += [guess]
+        result = query(testcase, guess)
+
+    print(testcase + "\t" + str(len(oldguesses)))
+    f = open("a.out", "a")
+    f.write(testcase + "\t" + str(len(oldguesses)))
+    f.write("\n")
+    f.close()
+
+
+
